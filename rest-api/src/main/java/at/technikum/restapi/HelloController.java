c05e567486a8d6d479a69f74d28fa23788cf965b
Felix Hadinger <felix.hadinger.2802@gmail.com>
2025-09-19 11:20:32 +0200

    Starting REST API branch

--- rest-api/src/main/java/at/technikum/restapi/HelloController.java
+++ rest-api/src/main/java/at/technikum/restapi/HelloController.java
@@ -3,6 +3,7 @@ package at.technikum.restapi;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// REST API Controller
 @RestController
 public class HelloController {
 
