d05a8f1debdb577003df6b87168021883b575110
Felix Hadinger <felix.hadinger.2802@gmail.com>
2025-09-21 15:34:17 +0200

    added some more unittests

--- rest-api/src/test/java/at/technikum/restapi/RestApiApplicationTests.java
+++ rest-api/src/test/java/at/technikum/restapi/RestApiApplicationTests.java
@@ -1,13 +1,18 @@
 package at.technikum.restapi;
 
+import at.technikum.restapi.persistence.DocumentEntity;
+import at.technikum.restapi.persistence.DocumentRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-// import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -16,9 +21,83 @@ class DocumentControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private DocumentRepository repository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private DocumentEntity savedDoc;
+
+    @BeforeEach
+    void setup() {
+        repository.deleteAll();
+        savedDoc = repository.save(DocumentEntity.builder()
+                .title("Test Title")
+                .build());
+    }
+
     @Test
     void testGetAllDocuments() throws Exception {
-        mockMvc.perform(get("/api/documents"))
-                .andExpect(status().isOk());
+        mockMvc.perform(get("/documents"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].title").value("Test Title"));
+    }
+
+    @Test
+    void testGetDocumentById_found() throws Exception {
+        mockMvc.perform(get("/documents/" + savedDoc.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value("Test Title"));
+    }
+
+    @Test
+    void testGetDocumentById_notFound() throws Exception {
+        mockMvc.perform(get("/documents/99999"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void testUploadDocument() throws Exception {
+        DocumentEntity newDoc = DocumentEntity.builder()
+                .title("New Document")
+                .build();
+
+        mockMvc.perform(post("/documents")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(newDoc)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value("New Document"));
+    }
+
+    @Test
+    void testUpdateDocument_found() throws Exception {
+        DocumentEntity updated = DocumentEntity.builder()
+                .title("Updated Title")
+                .build();
+
+        mockMvc.perform(put("/documents/" + savedDoc.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updated)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value("Updated Title"));
+    }
+
+    @Test
+    void testUpdateDocument_notFound() throws Exception {
+        DocumentEntity updated = DocumentEntity.builder()
+                .title("Does Not Exist")
+                .build();
+
+        mockMvc.perform(put("/documents/99999")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updated)))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void testDeleteDocument() throws Exception {
+        mockMvc.perform(delete("/documents/" + savedDoc.getId()))
+                .andExpect(status().isNoContent());
     }
 }
