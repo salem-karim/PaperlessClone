83d70a643cc280b5a32333134381be3a55901ad7
salem-karim <salem.karim@icloud.com>
2025-10-04 13:15:59 +0200

    fix: confirm RabbitMQ Java working

--- rest-api/src/main/java/at/technikum/restapi/rabbitMQ/DocumentPublisher.java
+++ rest-api/src/main/java/at/technikum/restapi/rabbitMQ/DocumentPublisher.java
@@ -2,26 +2,31 @@ package at.technikum.restapi.rabbitMQ;
 
 import at.technikum.restapi.service.DocumentDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
 import static at.technikum.restapi.rabbitMQ.RabbitConfig.*;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class DocumentPublisher {
 
     private final RabbitTemplate rabbitTemplate;
 
-    public void publishDocumentCreated(DocumentDto doc) {
+    public void publishDocumentCreated(final DocumentDto doc) {
+        log.info("Publishing document created event for: {}", doc.getTitle());
         rabbitTemplate.convertAndSend(EXCHANGE, "documents.created", doc);
     }
 
-    public void publishDocumentUpdated(DocumentDto doc) {
+    public void publishDocumentUpdated(final DocumentDto doc) {
+        log.info("Publishing document updated event for: {}", doc.getTitle());
         rabbitTemplate.convertAndSend(EXCHANGE, "documents.updated", doc);
     }
 
-    public void publishDocumentDeleted(String id) {
+    public void publishDocumentDeleted(final String id) {
+        log.info("Publishing document deleted event for ID: {}", id);
         rabbitTemplate.convertAndSend(EXCHANGE, "documents.deleted", id);
     }
 }
--- rest-api/src/main/java/at/technikum/restapi/rabbitMQ/RabbitConfig.java
+++ rest-api/src/main/java/at/technikum/restapi/rabbitMQ/RabbitConfig.java
@@ -1,28 +1,58 @@
 package at.technikum.restapi.rabbitMQ;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitConfig {
-
-    public static final String EXCHANGE = "documents.exchange";
-    public static final String QUEUE = "documents.queue";
-    public static final String ROUTING_KEY = "documents.#";
-
+    
+    // Exchange for document operations
+    public static final String EXCHANGE = "documents.operations";
+    
+    // Queue names for different operations
+    public static final String DOCUMENTS_QUEUE = "documents.processing";
+    
     @Bean
-    public TopicExchange exchange() {
+    public TopicExchange documentsExchange() {
         return new TopicExchange(EXCHANGE);
     }
 
     @Bean
-    public Queue queue() {
-        return new Queue(QUEUE, true);
+    public Queue documentsQueue() {
+        return new Queue(DOCUMENTS_QUEUE);
     }
 
     @Bean
-    public Binding binding(Queue queue, TopicExchange exchange) {
-        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    public Binding documentsBinding(Queue documentsQueue, TopicExchange documentsExchange) {
+        // Bind to all document operations
+        return BindingBuilder
+            .bind(documentsQueue)
+            .to(documentsExchange)
+            .with("documents.*");
+    }
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory("localhost");
+        connectionFactory.setUsername("guest");
+        connectionFactory.setPassword("guest");
+        return connectionFactory;
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(Jackson2JsonMessageConverter messageConverter) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory());
+        rabbitTemplate.setMessageConverter(messageConverter);
+        return rabbitTemplate;
     }
 }
